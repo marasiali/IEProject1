8a83f160e4731cb7600142efe9bf6568a7fe973a
m.dehghan <meghdadd78@gmail.com>
2021-12-20 18:08:22 +0330

    delay feedback implemented

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 
 class Server:
@@ -7,11 +8,14 @@ class Server:
         self.MCAST_GRP = '224.1.1.1'
         self.MCAST_PORT = 5007
         self.MULTICAST_TTL = 2
-        
+        self.clients = []
+
         while True:
             try:
-                self.ip = input('enter server IP: ')
-                self.port = int(input('enter server port number: '))
+                """ self.ip = input('enter server IP: ')
+                self.port = int(input('enter server port number: ')) """
+                self.ip = '127.0.0.1'
+                self.port = 8081
 
                 self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
                 self.sock.bind((self.ip, self.port))
@@ -27,10 +31,16 @@ class Server:
         while True:
             try:
                 data, addr = self.sock.recvfrom(1024)
+                client = Client.get_or_create(addr[0], addr[1])
+                delay = client.get_delay()
+                if delay > Client.threshold:
+                    self.send_delay_feedback(client)
+                
+                print('this is delay from server ', delay)
                 self.send_multicast(data)
                 print(str(len(data)) + ' bytes received from ' + str(addr))
             except Exception as e:
-                print('receiving data from client ' + str(addr) + ' failed: ' + str(e))
+               print('receiving data from client ' + str(addr) + ' failed: ' + str(e))
        
     def send_multicast(self, data):
         try:
@@ -38,6 +48,44 @@ class Server:
             print(str(len(data)) + ' bytes sent to ' + str(self.MCAST_GRP))
         except Exception as e:
             print('sending multicast data failed: ' + str(e))
+
+    def send_delay_feedback(self, client):
+        data = b'd'
+        self.sock.sendto(data, (client.ip, client.port))
+
+
+class Client():
+
+    connected_clients = []
+    threshold = 0.05
+
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.last_packet_timestamp = None
+
+    @classmethod
+    def get_or_create(cls, ip, port) -> 'Client':
+        print('size ', len(cls.connected_clients))
+        for client in cls.connected_clients:
+            if client.ip == ip and client.port == port:
+                return client
+        client = cls(ip, port)
+        cls.connected_clients.append(client)
+        return client
+
+    def get_delay(self):
+        if self.last_packet_timestamp == None:
+            self.last_packet_timestamp = time.time()
+            return 0
+        else:
+            real_time = time.time()
+            delay = real_time - self.last_packet_timestamp
+            self.last_packet_timestamp = real_time
+            """ print('real time ', real_time)
+            print('delat ', delay)
+            print('last packet updated ', self.last_packet_timestamp) """
+            return delay 
 
         
 def main():
--- singer.py
+++ singer.py
@@ -1,6 +1,8 @@
 import socket
-import pyaudio
 import wave
+import time
+import random
+import threading
 
 
 class Client:
@@ -8,45 +10,49 @@ class Client:
     def __init__(self):
         self.audio_folder = './audio/'
         self.chunk_size = 512   
-        self.audio_format = pyaudio.paInt16
         self.channels = 1
         self.rate = 44100
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
        
         while True:
             try:
-                self.server_ip = input('enter IP address of server: ')
+                """ self.server_ip = input('enter IP address of server: ')
                 self.server_port = int(input('enter target port of server: '))
-                self.file_name = input('enter .wav file name from audio folder: ')
+                self.file_name = input('enter .wav file name from audio folder: ') """
+                self.server_ip = '127.0.0.1'
+                self.server_port = 8081
+                self.file_name = 'a.wav'
                 break
             except:
                 print('incorrect input')
 
         self.wave_file = wave.open(self.audio_folder + self.file_name, 'rb')
-        
-        self.pyaudio = pyaudio.PyAudio()
-        self.recording_stream = self.pyaudio.open(
-            format=self.get_format_from_width(self.wave_file.getsampwidth()), 
-            channels=self.wave_file.getnchannels(), 
-            rate=self.wave_file.getframerate(), 
-            input=True, 
-            frames_per_buffer=self.chunk_size)
-
             
     def send(self):
         data = self.wave_file.readframes(self.chunk_size)
+        sample_rate = self.wave_file.getframerate()
         while data != b'':
             try:
+                delay = random.randint(0, 80) / 1000
+                time.sleep(0.8 * self.chunk_size / sample_rate)
                 self.socket.sendto(data, (self.server_ip, self.server_port))
-                print(data)
+                
+                print('this is delay from client ', delay)
                 print(str(len(data)) + ' bytes sent to ' + str(self.server_ip))
                 data = self.wave_file.readframes(self.chunk_size)
             except Exception as e:
                 print('sending data to server failed: ' + str(e))
 
+    def receive_delay_feedback(self):
+        while True:
+            data, addr = self.socket.recvfrom(1)
+            if data == b'd':
+                print('you have delay')
 
 def main():
     client = Client()
+    receiving_thread = threading.Thread(target=client.receive_delay_feedback)
+    receiving_thread.start()
     client.send()
 
 if __name__ == '__main__':
